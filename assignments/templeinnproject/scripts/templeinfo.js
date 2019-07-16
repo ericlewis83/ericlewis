08688a46f6d1a464595c3b77fca313b03f59134a
ericlewis83 <ericlewis83@byui.edu>
2019-07-15 20:40:03 -0600

    a

--- assignments/templeinnproject/scripts/templeinfo.js
+++ assignments/templeinnproject/scripts/templeinfo.js
@@ -1,11 +1,11 @@
 let info;
-const data = fetch('https://assignments\templeinnproject\scripts\templeinfo.json').then(res => res.json()).then(data => info = data)
+const data = fetch('\scripts\templeinfo.json').then(res => res.json()).then(data => info = data)
 
 var sectionSe = document.getElementById('boise');
 var sectionSp = document.getElementById('idahofalls');
 var sectionCr = document.getElementById('provo');
 var sectionPo = document.getElementById('saltlake');
-var requestURL = 'https://assignments\templeinnproject\scripts\templeinfo.json';
+var requestURL = 'https://ericlewis83.github.io/assignments/templeinnproject/scripts/templeinfo.json';
 
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
