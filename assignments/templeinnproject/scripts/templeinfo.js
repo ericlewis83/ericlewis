6b8d6d73838e146299c008e158d7fcbc2662696a
ericlewis83 <ericlewis83@byui.edu>
2019-07-19 22:27:17 -0600

    a

--- assignments/templeinnproject/scripts/templeinfo.js
+++ assignments/templeinnproject/scripts/templeinfo.js
@@ -3,7 +3,7 @@ let boiseS = document.getElementById("boise-services");
 let bName = document.getElementById("boise-name");
 let bAddr = document.getElementById("boise-addr");
 let bPhone = document.getElementById("boise-phone");
-let bEmail= document.getElementById("boise-email");
+let bEmail = document.getElementById("boise-email");
 let bOrdinance = document.getElementById("boise-ordinance")
 
 let provoC = document.getElementById("provo-closures");
@@ -37,82 +37,84 @@ request.open('GET', URL);
 request.responseType = 'json';
 request.send();
 request.onload = function () {
-  var templeData = request.response;
-  boiseInfo(templeData);
-  provoInfo(templeData);
-  fallsInfo(templeData);
-  slcInfo(templeData);
+    var templeData = request.response;
+    boiseInfo(templeData);
+    provoInfo(templeData);
+    fallsInfo(templeData);
+    slcInfo(templeData);
 
-  bName.innerHTML = templeData[0].name;
-  fName.innerHTML = templeData[1].name;
-  pName.innerHTML = templeData[2].name;
-  sName.innerHTML = templeData[3].name;
-
-
-  bAddr.innerHTML = templeData[0].address;
-  fAddr.innerHTML = templeData[1].address;
-  pAddr.innerHTML = templeData[2].address;
-  sAddr.innerHTML = templeData[3].address;
-
-  bPhone.innerHTML = templeData[0].phone;
-  fPhone.innerHTML = templeData[1].phone;
-  pPhone.innerHTML = templeData[2].phone;
-  sPhone.innerHTML = templeData[3].phone;
-
-  bEmail.innerHTML = templeData[0].email;
-  fEmail.innerHTML = templeData[1].email;
-  pEmail.innerHTML = templeData[2].email;
-  sEmail.innerHTML = templeData[3].email;
-
-  bOrdinance.innerHTML = templeData[0].ordinances;
-  fOrdinance.innerHTML = templeData[1].ordinances;
-  pOrdinance.innerHTML = templeData[2].ordinances;
-  sOrdinance.innerHTML = templeData[3].ordinances;  
-
-  boiseS.innerHTML = templeData[0].services;
-  fallsS.innerHTML = templeData[1].services;
-  provoS.innerHTML = templeData[2].services;
-  slcS.innerHTML = templeData[3].services;
-  
-}
+    bName.innerHTML = templeData[0].name;
+    fName.innerHTML = templeData[1].name;
+    pName.innerHTML = templeData[2].name;
+    sName.innerHTML = templeData[3].name;
 
 
-function boiseInfo(obj){
-let closures = obj[0].closures;
+    bAddr.innerHTML = templeData[0].address;
+    fAddr.innerHTML = templeData[1].address;
+    pAddr.innerHTML = templeData[2].address;
+    sAddr.innerHTML = templeData[3].address;
 
-for(let i=0; i< closures.length; i++){
-    let li = document.createElement("li");
-    let content = document.createTextNode(closures[i]);
-    li.appendChild(content);
-    boiseC.append(li);
+    bPhone.innerHTML = templeData[0].phone;
+    fPhone.innerHTML = templeData[1].phone;
+    pPhone.innerHTML = templeData[2].phone;
+    sPhone.innerHTML = templeData[3].phone;
+
+    bEmail.innerHTML = templeData[0].email;
+    fEmail.innerHTML = templeData[1].email;
+    pEmail.innerHTML = templeData[2].email;
+    sEmail.innerHTML = templeData[3].email;
+
+    bOrdinance.innerHTML = templeData[0].ordinances;
+    fOrdinance.innerHTML = templeData[1].ordinances;
+    pOrdinance.innerHTML = templeData[2].ordinances;
+    sOrdinance.innerHTML = templeData[3].ordinances;
+
+    boiseS.innerHTML = templeData[0].services;
+    fallsS.innerHTML = templeData[1].services;
+    provoS.innerHTML = templeData[2].services;
+    slcS.innerHTML = templeData[3].services;
 
 }
+
+
+function boiseInfo(obj) {
+    let closures = obj[0].closures;
+
+    for (let i = 0; i < closures.length; i++) {
+        let li = document.createElement("li");
+        let content = document.createTextNode(closures[i]);
+        li.appendChild(content);
+        boiseC.append(li);
+
+    }
 }
 
-function fallsInfo(obj){
+function fallsInfo(obj) {
     let closures = obj[1].closures;
-    for(let i=0; i< closures.length; i++){
+    for (let i = 0; i < closures.length; i++) {
         let li = document.createElement("li");
         let content = document.createTextNode(closures[i]);
         li.appendChild(content);
         fallsC.append(li);
     }
 }
-    function provoInfo(obj){
-        let closures = obj[2].closures;
-        for(let i=0; i< closures.length; i++){
-            let li = document.createElement("li");
-            let content = document.createTextNode(closures[i]);
-            li.appendChild(content);
-            provoC.append(li);
-        }
+
+function provoInfo(obj) {
+    let closures = obj[2].closures;
+    for (let i = 0; i < closures.length; i++) {
+        let li = document.createElement("li");
+        let content = document.createTextNode(closures[i]);
+        li.appendChild(content);
+        provoC.append(li);
     }
-        function slcInfo(obj){
-            let closures = obj[3].closures;
-            for(let i=0; i< closures.length; i++){
-                let li = document.createElement("li");
-                let content = document.createTextNode(closures[i]);
-                li.appendChild(content);
-                slcC.append(li);
-            }
-        }
+}
+
+function slcInfo(obj) {
+    let closures = obj[3].closures;
+    for (let i = 0; i < closures.length; i++) {
+        let li = document.createElement("li");
+        let content = document.createTextNode(closures[i]);
+        li.appendChild(content);
+        slcC.append(li);
+    }
+}
