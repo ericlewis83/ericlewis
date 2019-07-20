d006063e7059115a7234a0a68ae9b2a8c8b2adc4
ericlewis83 <ericlewis83@byui.edu>
2019-07-19 20:08:31 -0600

    a

--- assignments/templeinnproject/scripts/templeinfo.js
+++ assignments/templeinnproject/scripts/templeinfo.js
@@ -1,7 +1,30 @@
 let boiseC = document.getElementById("boise-closures");
+let boiseS = document.getElementById("boise-services");
+let bName = document.getElementById("boise-name");
+let bAddr = document.getElementById("boise-addr");
+let bPhone = document.getElementById("boise-phone");
+let bOrdinance = document.getElementById("boise-ordinance")
+
 let provoC = document.getElementById("provo-closures");
+let provoS = document.getElementById("provo-services");
+let pName = document.getElementById("provo-name");
+let pAddr = document.getElementById("provo-addr");
+let pPhone = document.getElementById("provo-phone");
+let pOrdinance = document.getElementById("provo-ordinance")
+
 let fallsC = document.getElementById("if-closures");
+let fallsS = document.getElementById("if-services");
+let fName = document.getElementById("if-name");
+let fAddr = document.getElementById("if-addr");
+let fPhone = document.getElementById("if-phone");
+let fOrdinance = document.getElementById("if-ordinance")
+
 let slcC = document.getElementById("slc-closures");
+let slcS = document.getElementById("slc-services");
+let sName = document.getElementById("slc-name");
+let sAddr = document.getElementById("slc-addr");
+let sPhone = document.getElementById("slc-phone");
+let sOrdinance = document.getElementById("slc-ordinance")
 
 let URL = 'https://ericlewis83.github.io/assignments/templeinnproject/scripts/templeinfo.json';
 var request = new XMLHttpRequest();
@@ -15,10 +38,39 @@ request.onload = function () {
   provoInfo(templeData);
   fallsInfo(templeData);
   slcInfo(templeData);
+
+  bName.innerHTML = templeData[0].name;
+  fName.innerHTML = templeData[1].name;
+  pName.innerHTML = templeData[2].name;
+  sName.innerHTML = templeData[3].name;
+
+
+  bAddr.innerHTML = templeData[0].address;
+  fAddr.innerHTML = templeData[1].address;
+  pAddr.innerHTML = templeData[2].address;
+  sAddr.innerHTML = templeData[3].address;
+
+  bPhone.innerHTML = templeData[0].phone;
+  fPhone.innerHTML = templeData[1].phone;
+  pPhone.innerHTML = templeData[2].phone;
+  sPhone.innerHTML = templeData[3].phone;
+
+  bOrdinance.innerHTML = templeData[0].ordinances;
+  fOrdinance.innerHTML = templeData[1].ordinances;
+  pOrdinance.innerHTML = templeData[2].ordinances;
+  sOrdinance.innerHTML = templeData[3].ordinances;  
+
+  boiseS.innerHTML = templeData[0].services;
+  fallsS.innerHTML = templeData[1].services;
+  provoS.innerHTML = templeData[2].services;
+  slcS.innerHTML = templeData[3].services;
+  
 }
+
 
 function boiseInfo(obj){
 let closures = obj[0].closures;
+
 for(let i=0; i< closures.length; i++){
     let li = document.createElement("li");
     let content = document.createTextNode(closures[i]);
@@ -55,161 +107,3 @@ function fallsInfo(obj){
                 slcC.append(li);
             }
         }
-
-    
-function populateBoise(templeInfo) {
-    var templeSe = templeInfo['temples'];
-   
-    for (var i = 0; i < templeSe.length; i++) {
-        var myArticle = document.createElement('article');
-        var myH2 = document.createElement('h2');
-        var myPara1 = document.createElement('p');
-        var myPara2 = document.createElement('p');
-        var myPara3 = document.createElement('p');
-        var myPara4 = document.createElement('p');
-        var myPara5 = document.createElement('p');
-        var myPara6 = document.createElement('p');
-        var myPara7 = document.createElement('p');
-        var myPara8 = document.createElement('p');
-
-        if (templeSe[0].name ==='Boise') {
-            myH2.textContent = templeSe[0].name;
-            myPara1.textContent = 'Address: ' + templeSe[0].address;
-            myPara2.textContent = 'Telephone: ' + templeSe[0].telephone;
-            myPara3.textContent = 'E-mail: ' + templeSe[0].email;
-            myPara4.textContent = 'Services: ' + templeSe[0].services;
-            myPara5.textContent = 'History: ' + templeSe[0].history;
-            myPara6.textContent = 'Ordinance Schedule: ' + templeSe[0].ordinanceSchedule;
-            myPara7.textContent = 'Session Schedule: ' + templeSe[0].sessionSchedule;
-            myPara8.textContent = 'Temple Closure Schedule: ' + templeSe[0].templeClosures;
-        }
-    }
-    myArticle.appendChild(myH2);
-    myArticle.appendChild(myPara1);
-    myArticle.appendChild(myPara2);
-    myArticle.appendChild(myPara3);
-    myArticle.appendChild(myPara4);
-    myArticle.appendChild(myPara5);
-    myArticle.appendChild(myPara6);
-    myArticle.appendChild(myPara7);
-    myArticle.appendChild(myPara8);
-
-    sectionSe.appendChild(myArticle);
-}
-
-function populateIdahoFalls(templeInfo) {
-    var templeSp = templeInfo['temples'];
-   
-    for (var i = 0; i < templeSp.length; i++) {
-        var myArticle = document.createElement('article');
-        var myH2 = document.createElement('h2');
-        var myPara1 = document.createElement('p');
-        var myPara2 = document.createElement('p');
-        var myPara3 = document.createElement('p');
-        var myPara4 = document.createElement('p');
-        var myPara5 = document.createElement('p');
-        var myPara6 = document.createElement('p');
-        var myPara7 = document.createElement('p');
-        var myPara8 = document.createElement('p');
-
-        if (templeSp[1].name ==='IdahoFalls') {
-            myH2.textContent = templeSp[1].name;
-            myPara1.textContent = 'Address: ' + templeSp[1].address;
-            myPara2.textContent = 'Telephone: ' + templeSp[1].telephone;
-            myPara3.textContent = 'E-mail: ' + templeSp[1].email;
-            myPara4.textContent = 'Services: ' + templeSp[1].services;
-            myPara5.textContent = 'History: ' + templeSp[1].history;
-            myPara6.textContent = 'Ordinance Schedule: ' + templeSp[1].ordinanceSchedule;
-            myPara7.textContent = 'Session Schedule: ' + templeSp[1].sessionSchedule;
-            myPara8.textContent = 'Temple Closure Schedule: ' + templeSp[1].templeClosures;
-        }
-    }
-    myArticle.appendChild(myH2);
-    myArticle.appendChild(myPara1);
-    myArticle.appendChild(myPara2);
-    myArticle.appendChild(myPara3);
-    myArticle.appendChild(myPara4);
-    myArticle.appendChild(myPara5);
-    myArticle.appendChild(myPara6);
-    myArticle.appendChild(myPara7);
-    myArticle.appendChild(myPara8);
-
-    sectionSp.appendChild(myArticle);
-}
-function populateProvo(templeInfo) {
-    var templeCr = templeInfo['temples'];
-   
-    for (var i = 0; i < templeCr.length; i++) {
-        var myArticle = document.createElement('article');
-        var myH2 = document.createElement('h2');
-        var myPara1 = document.createElement('p');
-        var myPara2 = document.createElement('p');
-        var myPara3 = document.createElement('p');
-        var myPara4 = document.createElement('p');
-        var myPara5 = document.createElement('p');
-        var myPara6 = document.createElement('p');
-        var myPara7 = document.createElement('p');
-        var myPara8 = document.createElement('p');
-
-        if (templeCr[2].name ==='Provo') {
-            myH2.textContent = templeCr[2].name;
-            myPara1.textContent = 'Address: ' + templeCr[2].address;
-            myPara2.textContent = 'Telephone: ' + templeCr[2].telephone;
-            myPara3.textContent = 'E-mail: ' + templeCr[2].email;
-            myPara4.textContent = 'Services: ' + templeCr[2].services;
-            myPara5.textContent = 'History: ' + templeCr[2].history;
-            myPara6.textContent = 'Ordinance Schedule: ' + templeCr[2].ordinanceSchedule;
-            myPara7.textContent = 'Session Schedule: ' + templeCr[2].sessionSchedule;
-            myPara8.textContent = 'Temple Closure Schedule: ' + templeCr[2].templeClosures;
-        }
-    }
-    myArticle.appendChild(myH2);
-    myArticle.appendChild(myPara1);
-    myArticle.appendChild(myPara2);
-    myArticle.appendChild(myPara3);
-    myArticle.appendChild(myPara4);
-    myArticle.appendChild(myPara5);
-    myArticle.appendChild(myPara6);
-    myArticle.appendChild(myPara7);
-    myArticle.appendChild(myPara8);
-
-    sectionCr.appendChild(myArticle);
-}
-function populateSaltLake(templeInfo) {
-    var templePo = templeInfo['temples'];
-   
-    for (var i = 0; i < templePo.length; i++) {
-        var myArticle = document.createElement('article');
-        var myH2 = document.createElement('h2');
-        var myPara1 = document.createElement('p');
-        var myPara2 = document.createElement('p');
-        var myPara3 = document.createElement('p');
-        var myPara4 = document.createElement('p');
-        var myPara5 = document.createElement('p');
-        var myPara6 = document.createElement('p');
-        var myPara7 = document.createElement('p');
-        var myPara8 = document.createElement('p');
-
-        if (templePo[3].name ==='Salt Lake') {
-            myH2.textContent = templePo[3].name;
-            myPara1.textContent = 'Address: ' + templePo[3].address;
-            myPara2.textContent = 'Telephone: ' + templePo[3].telephone;
-            myPara3.textContent = 'E-mail: ' + templePo[3].email;
-            myPara4.textContent = 'Services: ' + templePo[3].services;
-            myPara5.textContent = 'History: ' + templePo[3].history;
-            myPara6.textContent = 'Ordinance Schedule: ' + templePo[3].ordinanceSchedule;
-            myPara7.textContent = 'Session Schedule: ' + templePo[3].sessionSchedule;
-            myPara8.textContent = 'Temple Closure Schedule: ' + templePo[3].templeClosures;
-        }
-    }
-    myArticle.appendChild(myH2);
-    myArticle.appendChild(myPara1);
-    myArticle.appendChild(myPara2);
-    myArticle.appendChild(myPara3);
-    myArticle.appendChild(myPara4);
-    myArticle.appendChild(myPara5);
-    myArticle.appendChild(myPara6);
-    myArticle.appendChild(myPara7);
-    myArticle.appendChild(myPara8);
-
-    sectionPo.appendChild(myArticle);}
